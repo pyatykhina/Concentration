e1ccac323fca6ba9867b2e80acac6e8c8f3cc4bf
Tatiana Pyatykhina <tpyatyhina@mail.ru>
2018-10-19 13:01:04 +0300

    protocol

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -37,7 +37,7 @@ struct Concentration {
         assert(cards.indices.contains(index), "Concentration.chooseCard(at:\(index)): choosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                if cards[matchIndex].identifier == cards[index].identifier {
+                if cards[matchIndex] == cards[index] {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     private var emojiChoices = ["🐶", "🐱", "🐭", "🐰", "🦊", "🐻", "🐶", "🐱", "🐭", "🐰", "🦊", "🐻"]
     
-    var emoji = [Int: String] ()
+    private var emoji = [Card: String]()
     
     private(set) var flipCount = 0 {
         didSet {
@@ -56,10 +56,10 @@ class ViewController: UIViewController {
     }
 
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-                emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+                emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
             }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
 }
 
